475b2b98c62ef887e6efe867e61693ef5fe5b6d8
UmidMuzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2022-06-18 15:49:44 -0700

    Update and rename Mad Libs to mad_libs/move.py

---
+++ mad_libs/move.py
@@ -0,0 +1,25 @@
+### -*- coding: utf-8 -*-
+###
+### Author: Umidjon Muzrapov
+### Class: CSc 110
+### Description: Plugs in given values in the text.
+###
+###
+female_name=input('''A female name:
+''')
+street_name=input('''A street name:
+''')
+male_name=input('''A male name:
+''')
+object1=input('''An object:
+''')
+vehicle=input('''A vehicle:
+''')
+adjective=input('''An adjective:
+''')
+print('----------')
+print(female_name, 'decided to move from her apartment on 5th')
+print('to a condo on', street_name + '.','She called her friend', male_name)
+print('for help. However, they could not fit', object1,'into')
+print('the moving truck, so they had to rent a', adjective, vehicle)
+print('in order to move it!')
