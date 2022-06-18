ac718d9bf5abdbf8f19f35985a92057015271b62
UmidMuzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2022-06-18 15:50:47 -0700

    Add files via upload

---
+++ mad_libs/vacation.py
@@ -0,0 +1,22 @@
+### -*- coding: utf-8 -*-
+###
+### Author: Umidjon Muzrapov
+### Class: CSc 110
+### Description: Plugs in given values in the text.
+###
+###
+male_n=input("A male name:\n")
+female_n=input("A female name:\n")
+pet_n=input("A pet name:\n")
+place=input("A place:\n")
+adj=input("An adjective:\n")
+animal=input("An animal:\n")
+verbing=input("A verb ending in ing:\n")
+adverb=input("An adverb:\n")
+print("----------")
+print(male_n,"and",female_n,"were best friends.")
+print("One day", male_n, "and", female_n, "decided to go on a")
+print("vacation to", place + ".","However, they didn't know")
+print("what to do with their",adj,"pet", animal,"named", pet_n + ".")
+print(pet_n, "had been causing problems, due to constant", verbing + ".")
+print(male_n, "found a sitter for their pet, and", adverb, "went on the trip.")
